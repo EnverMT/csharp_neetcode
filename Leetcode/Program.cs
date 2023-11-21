d236bc4df2657c705ce5e095409994f1c5699bb5
DevPrima <DevPrima@mail.com>
2023-11-21 11:59:58 +0500

    Add project files.

---
+++ Leetcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using Leetcode.Stack._853_Car_fleet;
+
+namespace Leetcode;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Test solution = new Test();
+        solution.TestCases();
+    }
+}
---
+++ Leetcode/Stack/_853_Car_fleet/Solution.cs
@@ -0,0 +1,43 @@
+﻿// https://leetcode.com/problems/car-fleet/
+namespace Leetcode.Stack._853_Car_fleet;
+
+public class Solution
+{
+    private Stack<Car> m_stack = new();
+    private List<Car> cars = new();
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        for (int i = 0; i < position.Length; i++)
+        {
+            double reachTime = (double)(target - position[i]) / speed[i];
+            Car car = new Car(position[i], speed[i], reachTime);
+            cars.Add(car);
+        }
+
+        List<Car> orderedCars = cars.OrderByDescending(car => car.pos).ToList();
+
+        foreach (Car car in orderedCars)
+        {
+            if (0 == m_stack.Count || m_stack.Peek().reachTime < car.reachTime)
+            {
+                m_stack.Push(car);
+            }
+        }
+
+        return m_stack.Count;
+    }
+}
+
+public class Car
+{
+    public int pos;
+    public int speed;
+    public double reachTime;
+
+    public Car(int pos, int speed, double reachTime)
+    {
+        this.pos = pos;
+        this.speed = speed;
+        this.reachTime = reachTime;
+    }
+}
