cc55ed4c7132e848530c03f5800ca63cdbd0ea1f
EnverMT <DevPrima@mail.com>
2024-01-08 18:37:06 +0500

    search 2d matrix

---
+++ LeetCode.Tests/Binary search/74_search_2d_matrix.cs
@@ -0,0 +1,26 @@
+﻿using Leetcode.Binary_Search._74_search_2d_matrix;
+
+namespace Leetcode.Tests.Binary_search;
+
+public class _74_search_2d_matrix
+{
+    private readonly Solution _solution;
+    public _74_search_2d_matrix()
+    {
+        _solution = new Solution();
+    }
+
+    [Fact]
+    public void Stack_search_2d_matrix_1()
+    {
+        bool result = _solution.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Stack_search_2d_matrix_2()
+    {
+        bool result = _solution.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13);
+        Assert.False(result);
+    }
+}
---
+++ Leetcode/Binary Search/74_search_2d_matrix/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode.Binary_Search._74_search_2d_matrix;
+
+public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        if (matrix.Any(x => x.Contains(target))) return true;
+
+        return false;
+    }
+}
