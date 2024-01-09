b398c59c9602259ec8db029728cb7cb7454a4655
EnverMT <DevPrima@mail.com>
2024-01-09 12:57:00 +0500

    153.find min in rotated array

---
+++ LeetCode.Tests/Binary search/153_find_minimum_in_rotated_array.cs
@@ -0,0 +1,46 @@
+﻿using Leetcode.Binary_Search._153_find_minimum_in_rotated_array;
+
+namespace Leetcode.Tests.Binary_search;
+
+public class _153_find_minimum_in_rotated_array
+{
+    private readonly Solution solution;
+    public _153_find_minimum_in_rotated_array()
+    {
+        this.solution = new Solution();
+    }
+
+    [Fact]
+    public void BinarySearch_MinInRotatedArray_1()
+    {
+        int result = solution.FindMin([3, 4, 5, 1, 2]);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void BinarySearch_MinInRotatedArray_2()
+    {
+        int result = solution.FindMin([4, 5, 6, 7, 0, 1, 2]);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void BinarySearch_MinInRotatedArray_3()
+    {
+        int result = solution.FindMin([11, 13, 15, 17]);
+
+        Assert.Equal(11, result);
+    }
+
+    [Fact]
+    public void BinarySearch_MinInRotatedArray_4()
+    {
+        int result = solution.FindMin([2,1]);
+
+        Assert.Equal(1, result);
+    }
+
+
+}
---
+++ Leetcode/Binary Search/153_find_minimum_in_rotated_array/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.Binary_Search._153_find_minimum_in_rotated_array;
+
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        int indexL = 0, indexR = nums.Length - 1, indexM = 0;
+
+        while (indexL < indexR)
+        {
+            indexM = (indexL + indexR) / 2;
+
+            if (nums[indexM] > nums[indexR])
+            {
+                indexL = indexM + 1;
+            } else
+            {
+                indexR = indexM;
+            }
+        }
+        GC.Collect();
+        return nums[indexL];
+    }
+}
