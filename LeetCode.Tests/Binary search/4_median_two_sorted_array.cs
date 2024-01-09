eef608146007e1c6694f1cbf504fb43fcf183a21
EnverMT <DevPrima@mail.com>
2024-01-09 16:19:51 +0500

    4. median of sorted two arrays - not complete

---
+++ LeetCode.Tests/Binary search/4_median_two_sorted_array.cs
@@ -0,0 +1,28 @@
+﻿using Leetcode.Binary_Search._4_median_two_sorted_array;
+
+namespace Leetcode.Tests.Binary_search;
+
+public class _4_median_two_sorted_array
+{
+    private readonly Solution solution;
+    public _4_median_two_sorted_array()
+    {
+        solution = new Solution();
+    }
+
+    [Fact]
+    public void BinarySearch_Median_1()
+    {
+        double result = solution.FindMedianSortedArrays([1, 3], [2]);
+
+        Assert.Equal(2.0, result);
+    }
+
+    [Fact]
+    public void BinarySearch_Median_2()
+    {
+        double result = solution.FindMedianSortedArrays([1, 2], [3, 4]);
+
+        Assert.Equal(2.5, result);
+    }
+}
---
+++ Leetcode/Binary Search/4_median_two_sorted_array/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace Leetcode.Binary_Search._4_median_two_sorted_array;
+
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        return 0;
+    }
+}
