481cb32a07fd22b7d9a17096d28b2d3a016809a8
EnverMT <DevPrima@mail.com>
2024-01-09 11:50:24 +0500

    875.Koko eat banana

---
+++ LeetCode.Tests/Binary search/875_Koko_eating_bananas.cs
@@ -0,0 +1,52 @@
+﻿using Leetcode.Binary_Search._875_Koko_eating_bananas;
+
+namespace Leetcode.Tests.Binary_search;
+
+public class _875_Koko_eating_bananas
+{
+    private readonly Solution _solution;
+    public _875_Koko_eating_bananas()
+    {
+        _solution = new Solution();
+    }
+
+    [Fact]
+    public void Stack_Koko_eat_bananas_1()
+    {
+        int result = _solution.MinEatingSpeed([3, 6, 7, 11], 8);
+
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void Stack_Koko_eat_bananas_2()
+    {
+        int result = _solution.MinEatingSpeed([30, 11, 23, 4, 20], 5);
+
+        Assert.Equal(30, result);
+    }
+
+    [Fact]
+    public void Stack_Koko_eat_bananas_3()
+    {
+        int result = _solution.MinEatingSpeed([30, 11, 23, 4, 20], 6);
+
+        Assert.Equal(23, result);
+    }
+
+    [Fact]
+    public void Stack_Koko_eat_bananas_4()
+    {
+        int result = _solution.MinEatingSpeed([312884470], 312884469);
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Stack_Koko_eat_bananas_5()
+    {
+        int result = _solution.MinEatingSpeed([805306368, 805306368, 805306368], 1000000000);
+
+        Assert.Equal(3, result);
+    }
+}
---
+++ Leetcode/Binary Search/875_Koko_eating_bananas/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode.Binary_Search._875_Koko_eating_bananas;
+
+public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        int indexL = 1;
+        int indexR = piles.Max();
+
+        int eatSpeed, totalTime;
+        int result = indexR;
+
+        while (indexL <= indexR)
+        {
+            eatSpeed = (indexL + indexR) / 2;
+            totalTime = this.GetEatHours(piles, eatSpeed);
+            if (totalTime < 0) break;
+
+
+            if (totalTime <= h)
+            {
+                result = eatSpeed;
+                indexR = eatSpeed - 1;
+            }
+            if (totalTime > h) { indexL = eatSpeed + 1; }
+
+        }
+        GC.Collect();
+        return result;
+    }
+
+    private int GetEatHours(int[] piles, int eatSpeed)
+    {
+        double totalTime = 0;
+        for (int i = 0; i < piles.Length; i++)
+        {
+            totalTime += Math.Ceiling((double)piles[i] / (double)eatSpeed);
+        }
+        return (int)totalTime;
+    }
+}
