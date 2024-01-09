733ab2f9160243f7e3b602a48c3f5225b09ad240
EnverMT <DevPrima@mail.com>
2024-01-09 15:49:26 +0500

    v2

--- Leetcode/Binary Search/981_Time_based_kv_store/Solution.cs
+++ Leetcode/Binary Search/981_Time_based_kv_store/Solution.cs
@@ -2,7 +2,7 @@
 
 public class TimeMap
 {
-    private readonly Dictionary<string, Dictionary<int, string>> timeMap;
+    private readonly Dictionary<string, List<(int, string)>> timeMap; // key : tuple (timestamp, value)
     public TimeMap()
     {
         this.timeMap = new();
@@ -12,25 +12,41 @@ public class TimeMap
     {
         if (this.timeMap.ContainsKey(key))
         {
-            this.timeMap[key].Add(timestamp, value);
+            this.timeMap[key].Add((timestamp, value));
         }
         else
         {
             this.timeMap[key] = new() {
-                { timestamp, value }
+                { (timestamp, value) }
             };
         }
     }
 
     public string Get(string key, int timestamp)
     {
-        return this.timeMap[key].Where(x => x.Key <= timestamp).OrderByDescending(x => x.Key).FirstOrDefault().Value ?? "";
+        if (!this.timeMap.ContainsKey(key)) return string.Empty;
+
+        List<(int Timestamp, string Value)> values = this.timeMap[key];
+
+        int l = 0, m, r = values.Count - 1;
+
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (values[m].Timestamp == timestamp) return values[m].Value;
+
+            if (values[m].Timestamp > timestamp)
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        if (l == 0) return string.Empty;
+
+        return values[l - 1].Value;
     }
 }
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap obj = new TimeMap();
- * obj.Set(key,value,timestamp);
- * string param_2 = obj.Get(key,timestamp);
- */
