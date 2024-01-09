6a1378e90f2a6122484610cae1f13d7253e1fb01
EnverMT <DevPrima@mail.com>
2024-01-09 14:02:04 +0500

    33. Search in rotated sorted array

---
+++ LeetCode.Tests/Binary search/33_search_in_rotated_sorted_array.cs
@@ -0,0 +1,36 @@
+﻿using Leetcode.Binary_Search._33_search_in_rotated_sorted_array;
+
+namespace Leetcode.Tests.Binary_search;
+
+public class _33_search_in_rotated_sorted_array
+{
+    private readonly Solution solution;
+    public _33_search_in_rotated_sorted_array()
+    {
+        this.solution = new Solution();
+    }
+
+    [Fact]
+    public void BinarySearch_SearchInRotatedArray_1()
+    {
+        int result = this.solution.Search([4, 5, 6, 7, 0, 1, 2], 0);
+
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void BinarySearch_SearchInRotatedArray_2()
+    {
+        int result = this.solution.Search([4, 5, 6, 7, 0, 1, 2], 3);
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void BinarySearch_SearchInRotatedArray_3()
+    {
+        int result = this.solution.Search([1], 0);
+
+        Assert.Equal(-1, result);
+    }
+}
---
+++ Leetcode/Binary Search/33_search_in_rotated_sorted_array/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode.Binary_Search._33_search_in_rotated_sorted_array;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1, m;
+
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (nums[m] == target) return m;
+
+            if (nums[l] <= nums[m])
+            { // left half sorted
+                if (nums[l] <= target && target <= nums[m])
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+            else
+            { // right half sorted
+                if (nums[m]<=target && target <= nums[r])
+                {
+                    l = m + 1;
+                } else
+                {
+                    r = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
