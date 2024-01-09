d7fa5d824bd0d2614f49e02603e0444b9272fcf9
EnverMT <DevPrima@mail.com>
2024-01-09 14:35:48 +0500

    981. time based kv store (v1)

---
+++ LeetCode.Tests/Binary search/981_Time_based_kv_store.cs
@@ -0,0 +1,24 @@
+﻿using Leetcode.Binary_Search._981_Time_based_kv_store;
+
+namespace Leetcode.Tests.Binary_search;
+
+public class _981_Time_based_kv_store
+{
+    private readonly TimeMap timeMap;
+    public _981_Time_based_kv_store()
+    {
+        timeMap = new TimeMap();
+    }
+
+    [Fact]
+    public void BinarySearch_TimeMap_1()
+    {
+        timeMap.Set("foo", "bar", 1);
+        Assert.Equal("bar", timeMap.Get("foo", 1));
+        Assert.Equal("bar", timeMap.Get("foo", 3));
+
+        timeMap.Set("foo", "bar2", 4);
+        Assert.Equal("bar2", timeMap.Get("foo", 4));
+        Assert.Equal("bar2", timeMap.Get("foo", 5));
+    }
+}
---
+++ Leetcode/Binary Search/981_Time_based_kv_store/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.Binary_Search._981_Time_based_kv_store;
+
+public class TimeMap
+{
+    private readonly Dictionary<string, Dictionary<int, string>> timeMap;
+    public TimeMap()
+    {
+        this.timeMap = new();
+    }
+
+    public void Set(string key, string value, int timestamp)
+    {
+        if (this.timeMap.ContainsKey(key))
+        {
+            this.timeMap[key].Add(timestamp, value);
+        }
+        else
+        {
+            this.timeMap[key] = new() {
+                { timestamp, value } 
+            };
+        }
+    }
+
+    public string Get(string key, int timestamp)
+    {
+        return this.timeMap[key].Where(x => x.Key <= timestamp).OrderByDescending(x => x.Key).FirstOrDefault().Value ?? "";
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.Set(key,value,timestamp);
+ * string param_2 = obj.Get(key,timestamp);
+ */
