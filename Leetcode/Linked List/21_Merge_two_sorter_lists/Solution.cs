b1683121a95103511bfd8e52f6637a0b9d7d69e6
Enver Toleuov <anvarmtg@gmail.com>
2024-04-03 12:23:42 +0500

    merge two list

---
+++ Leetcode/Linked List/21_Merge_two_sorter_lists/Solution.cs
@@ -0,0 +1,38 @@
+﻿using Leetcode.Linked_List._206_Reverse_linked_list;
+
+namespace Leetcode.Linked_List._21_Merge_two_sorter_lists;
+public class Solution
+{
+    private ListNode? head = new();
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode? tail = this.head;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        if (list1 != null)
+        {
+            tail.next = list1;
+        }
+        else
+        {
+            tail.next = list2;
+        }
+
+        return this.head?.next;
+    }
+}
