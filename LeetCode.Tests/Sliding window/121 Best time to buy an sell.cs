aef44f0533888ea8939bdc560bae69f6ee2c2393
EnverMT <anvarmtg@gmail.com>
2024-05-28 12:33:16 +0500

    solution 121

--- LeetCode.Tests/Sliding window/121 Best time to buy an sell.cs
+++ LeetCode.Tests/Sliding window/121 Best time to buy an sell.cs
@@ -1,4 +1,5 @@
 ﻿using Leetcode.SlidingWindow._121_Best_Time_to_Buy_and_Sell_Stock;
+using System.Diagnostics;
 
 namespace Leetcode.Tests.Sliding_window;
 
@@ -28,5 +29,23 @@ public class _121_Best_time_to_buy_an_sell
     public void LinkedList_143_Reorder_list_Test_4()
     {
         Assert.Equal(4, solution.MaxProfit([7, 3, 5, 2, 6, 0]));
+    }
+
+    [Fact]
+    public void LinkedList_143_Reorder_list_Test_5()
+    {
+        int[] prices = [7, 1, 5, 3, 6, 4];
+        int result = 5;
+        
+        Assert.Equal(result, solution.MaxProfit(prices));
+    }
+
+    [Fact]
+    public void LinkedList_143_Reorder_list_Test_6()
+    {
+        int[] prices = [7, 6, 4, 3, 1];
+        int result = 0;
+
+        Assert.Equal(result, solution.MaxProfit(prices));
     }
 }
--- Leetcode/SlidingWindow/121 Best Time to Buy and Sell Stock/Solution.cs
+++ Leetcode/SlidingWindow/121 Best Time to Buy and Sell Stock/Solution.cs
@@ -5,6 +5,23 @@ public class Solution
 {
     public int MaxProfit(int[] prices)
     {
-        return 0;
+        int maxProfit = 0;
+        int profit;
+        int indexL = 0, indexR = 1;
+
+        while (indexR <= prices.Length - 1)
+        {
+            if (prices[indexL] > prices[indexR])
+                indexL = indexR;
+
+            profit = prices[indexR] - prices[indexL];
+
+            if (maxProfit < profit)
+                maxProfit = profit;
+
+            indexR++;
+        }
+
+        return maxProfit;
     }
 }
