f5f16a6ee57a2ded3295c189008955c38024be05
EnverMT <anvarmtg@gmail.com>
2024-05-28 13:07:33 +0500

    longest substring

---
+++ LeetCode.Tests/Sliding window/3 Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,44 @@
+﻿using Leetcode.SlidingWindow._3_Longest_Substring_Without_Repeating_Characters;
+
+namespace Leetcode.Tests.Sliding_window;
+
+public class _3_Longest_Substring_Without_Repeating_Characters
+{
+    private readonly Solution solution = new();
+
+    [Fact]
+    public void _3_Longest_Substring_Without_Repeating_Characters_1()
+    {
+        string s = "zxczxc";
+        int result = 3;
+
+        Assert.Equal(result, solution.LengthOfLongestSubstring(s));
+    }
+
+    [Fact]
+    public void _3_Longest_Substring_Without_Repeating_Characters_2()
+    {
+        string s = "abcabcbb";
+        int result = 3;
+
+        Assert.Equal(result, solution.LengthOfLongestSubstring(s));
+    }
+
+    [Fact]
+    public void _3_Longest_Substring_Without_Repeating_Characters_3()
+    {
+        string s = "bbbbb";
+        int result = 1;
+
+        Assert.Equal(result, solution.LengthOfLongestSubstring(s));
+    }
+
+    [Fact]
+    public void _3_Longest_Substring_Without_Repeating_Characters_4()
+    {
+        string s = "pwwkew";
+        int result = 3;
+
+        Assert.Equal(result, solution.LengthOfLongestSubstring(s));
+    }
+}
---
+++ Leetcode/SlidingWindow/3 Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.SlidingWindow._3_Longest_Substring_Without_Repeating_Characters;
+
+public class Solution
+{
+    Queue<char> queue = new();
+
+    public int LengthOfLongestSubstring(string s)
+    {
+        int maxLength = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            while (queue.Contains(s.ElementAt(i)))
+            {
+                queue.Dequeue();
+            }
+
+            queue.Enqueue(s.ElementAt(i));
+
+            if (maxLength < queue.Count)
+                maxLength = queue.Count;
+        }
+
+        return maxLength;
+    }
+}
