830334086152d8b4c652d25d2c59ec8dc608f244
EnverMT <anvarmtg@gmail.com>
2024-05-29 11:24:37 +0500

    tests1

--- LeetCode.Tests/Tree/226 Invert Binary Tree.cs
+++ LeetCode.Tests/Tree/226 Invert Binary Tree.cs
@@ -1,5 +1,6 @@
-﻿using Leetcode.Tree._226_Invert_Binary_Tree;
-using Leetcode.Tree;
+﻿using Leetcode.Tree;
+using Leetcode.Tree._226_Invert_Binary_Tree;
+
 
 namespace Leetcode.Tests.Tree;
 
@@ -15,7 +16,58 @@ public class _226_Invert_Binary_Tree
         int[] root = [1, 2, 3];
         int[] output = [1, 3, 2];
 
-        TreeNode? rootTreeNode = _base.ConvertArrayToTree([1, 2, 3]);
+        TreeNode? rootTreeNode = _base.ConvertArrayToTree(root);
+        Assert.NotNull(rootTreeNode);
+
+        TreeNode outputTreeNode = _solution.InvertTree(rootTreeNode);
+        Assert.NotNull(outputTreeNode);
+
+        int[] outputArray = _base.ConvertTreeToArray(outputTreeNode);
+
+        Assert.Equal(output, outputArray);
+    }
+
+    [Fact]
+    public void _226_Invert_Binary_Tree_Test_2()
+    {
+        int[] root = [4, 2, 7, 1, 3, 6, 9];
+        int[] output = [4, 7, 2, 9, 6, 3, 1];
+
+        TreeNode? rootTreeNode = _base.ConvertArrayToTree(root);
+        Assert.NotNull(rootTreeNode);
+
+        TreeNode outputTreeNode = _solution.InvertTree(rootTreeNode);
+        Assert.NotNull(outputTreeNode);
+
+        int[] outputArray = _base.ConvertTreeToArray(outputTreeNode);
+
+        Assert.Equal(output, outputArray);
+    }
+    
+    [Fact]
+    public void _226_Invert_Binary_Tree_Test_3()
+    {
+        int[] root = [];
+        int[] output = [];
+
+        TreeNode? rootTreeNode = _base.ConvertArrayToTree(root);
+        Assert.NotNull(rootTreeNode);
+
+        TreeNode outputTreeNode = _solution.InvertTree(rootTreeNode);
+        Assert.NotNull(outputTreeNode);
+
+        int[] outputArray = _base.ConvertTreeToArray(outputTreeNode);
+
+        Assert.Equal(output, outputArray);
+    }
+
+    [Fact]
+    public void _226_Invert_Binary_Tree_Test_4()
+    {
+        int[] root = [1, 2, 3, 4, 5, 6, 7];
+        int[] output = [1, 3, 2, 7, 6, 5, 4];
+
+        TreeNode? rootTreeNode = _base.ConvertArrayToTree(root);
         Assert.NotNull(rootTreeNode);
 
         TreeNode outputTreeNode = _solution.InvertTree(rootTreeNode);
