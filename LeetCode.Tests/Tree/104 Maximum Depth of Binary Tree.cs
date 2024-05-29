7b0b5afb8ad1f1d3a4bb7a6d18ec292e1d61c012
EnverMT <anvarmtg@gmail.com>
2024-05-29 13:24:56 +0500

    tests

--- LeetCode.Tests/Tree/104 Maximum Depth of Binary Tree.cs
+++ LeetCode.Tests/Tree/104 Maximum Depth of Binary Tree.cs
@@ -30,4 +30,40 @@ public class _104_Maximum_Depth_of_Binary_Tree
 
         Assert.Equal(output, result);
     }
+
+    [Fact]
+    public void _104_Maximum_Depth_of_Binary_Tree_Test_3()
+    {
+        int?[] root = [3, 9, 20, null, null, 15, 7];
+        int result = 3;
+
+        TreeNode? rootTreeNode = Base.ConvertArrayToTree(root);
+        int output = _solution.MaxDepth(rootTreeNode);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void _104_Maximum_Depth_of_Binary_Tree_Test_4()
+    {
+        int?[] root = [1, 2, 3, null, null, 4];
+        int result = 3;
+
+        TreeNode? rootTreeNode = Base.ConvertArrayToTree(root);
+        int output = _solution.MaxDepth(rootTreeNode);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void _104_Maximum_Depth_of_Binary_Tree_Test_5()
+    {
+        int?[] root = [];
+        int result = 0;
+
+        TreeNode? rootTreeNode = Base.ConvertArrayToTree(root);
+        int output = _solution.MaxDepth(rootTreeNode);
+
+        Assert.Equal(output, result);
+    }
 }
