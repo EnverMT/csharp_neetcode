9862f2b4f74a9ffc3025c17fb222bb43629e6a7d
EnverMT <anvarmtg@gmail.com>
2024-05-29 12:26:01 +0500

    v2

--- Leetcode/Tree/226 Invert Binary Tree/Solution.cs
+++ Leetcode/Tree/226 Invert Binary Tree/Solution.cs
@@ -4,28 +4,14 @@ public class Solution
 {
     public TreeNode? InvertTree(TreeNode? root)
     {
-        if (root == null)
-            return root;
+        if (root == null) return null;
 
-        Queue<TreeNode> queue = new Queue<TreeNode>();
-        queue.Enqueue(root);
+        TreeNode? left = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(left);
 
-        while (queue.Count > 0)
-        {
-            TreeNode? current = queue.Dequeue();
+        GC.Collect();
 
-            // Swap the left and right children
-            TreeNode? temp = current.left;
-            current.left = current.right;
-            current.right = temp;
-
-            if (current.left != null)
-                queue.Enqueue(current.left);
-
-            if (current.right != null)
-                queue.Enqueue(current.right);
-        }
-
-        return root;
+        return root;        
     }
 }
