2c980cd8deab054f9041fe87ab06119612d8bd86
EnverMT <anvarmtg@gmail.com>
2024-05-30 16:04:19 +0500

    cleanup
    gc
    
    solution
    
    424 longest repqating

---
+++ LeetCode.Tests/Sliding window/424 Longest Repeating Character Replacement.cs
@@ -0,0 +1,48 @@
+﻿using Leetcode.SlidingWindow._424_Longest_Repeating_Character_Replacement;
+
+namespace Leetcode.Tests.Sliding_window;
+
+public class _424_Longest_Repeating_Character_Replacement
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void _424_Longest_Repeating_Character_Replacement_Test_1()
+    {
+        string s = "ABAB";
+        int k = 2;
+        int expectedOutput = 4;
+
+        Assert.Equal(expectedOutput, _solution.CharacterReplacement(s, k));
+    }
+
+    [Fact]
+    public void _424_Longest_Repeating_Character_Replacement_Test_2()
+    {
+        string s = "AABABBA";
+        int k = 1;
+        int expectedOutput = 4;
+
+        Assert.Equal(expectedOutput, _solution.CharacterReplacement(s, k));
+    }
+
+    [Fact]
+    public void _424_Longest_Repeating_Character_Replacement_Test_3()
+    {
+        string s = "XYYX";
+        int k = 2;
+        int expectedOutput = 4;
+
+        Assert.Equal(expectedOutput, _solution.CharacterReplacement(s, k));
+    }
+
+    [Fact]
+    public void _424_Longest_Repeating_Character_Replacement_Test_4()
+    {
+        string s = "AAABABB";
+        int k = 1;
+        int expectedOutput = 5;
+
+        Assert.Equal(expectedOutput, _solution.CharacterReplacement(s, k));
+    }
+}
---
+++ Leetcode/SlidingWindow/424 Longest Repeating Character Replacement/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace Leetcode.SlidingWindow._424_Longest_Repeating_Character_Replacement;
+
+public class Solution
+{
+    private readonly Dictionary<char, int> _frequence = new();
+
+    public int CharacterReplacement(string s, int k)
+    {
+        int indexL = 0;
+        int result = 0;
+
+        for (int indexR = 0; indexR < s.Length; indexR++)
+        {
+            if (_frequence.ContainsKey(s[indexR]))
+                _frequence[s[indexR]] += 1;
+            else
+                _frequence.Add(s[indexR], 1);
+
+            while (indexR - indexL + 1 - GetMax() > k)
+            {
+                _frequence[s[indexL]] -= 1;
+                indexL++;
+            }
+
+            result = Math.Max(result, indexR - indexL + 1);
+        }
+
+        GC.Collect();
+
+        return result;
+    }
+
+    private int GetMax()
+    {
+        int max = 0;
+
+        foreach (var item in _frequence)
+        {
+            if (max < item.Value)
+                max = item.Value;
+        }
+
+        return max;
+    }
+}
