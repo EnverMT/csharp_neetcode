baf54bdb1ead03ff5e5c6a68c83db1743361f762
EnverMT <anvarmtg@gmail.com>
2024-05-31 11:47:28 +0500

    permutation in string

---
+++ LeetCode.Tests/Sliding window/567 Permutation in String.cs
@@ -0,0 +1,59 @@
+﻿using Leetcode.SlidingWindow._567_Permutation_in_String;
+
+
+namespace Leetcode.Tests.Sliding_window;
+
+public class _567_Permutation_in_String
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void _567_Permutation_in_String_Test_1()
+    {
+        string s1 = "ab";
+        string s2 = "eidbaooo";
+        bool output = true;
+
+        Assert.Equal(output, _solution.CheckInclusion(s1, s2));
+    }
+
+    [Fact]
+    public void _567_Permutation_in_String_Test_2()
+    {
+        string s1 = "ab";
+        string s2 = "eidboaoo";
+        bool output = false;
+
+        Assert.Equal(output, _solution.CheckInclusion(s1, s2));
+    }
+
+    [Fact]
+    public void _567_Permutation_in_String_Test_3()
+    {
+        string s1 = "abc";
+        string s2 = "lecabee";
+        bool output = true;
+
+        Assert.Equal(output, _solution.CheckInclusion(s1, s2));
+    }
+
+    [Fact]
+    public void _567_Permutation_in_String_Test_4()
+    {
+        string s1 = "abc";
+        string s2 = "lecaabee";
+        bool output = false;
+
+        Assert.Equal(output, _solution.CheckInclusion(s1, s2));
+    }
+
+    [Fact]
+    public void _567_Permutation_in_String_Test_5()
+    {
+        string s1 = "adc";
+        string s2 = "dcda";
+        bool output = true;
+
+        Assert.Equal(output, _solution.CheckInclusion(s1, s2));
+    }
+}
---
+++ Leetcode/SlidingWindow/567 Permutation in String/Solution.cs
@@ -0,0 +1,44 @@
+﻿namespace Leetcode.SlidingWindow._567_Permutation_in_String;
+
+public class Solution
+{
+    private Dictionary<char, int>? _dict1;
+    private readonly Dictionary<char, int> _dict2 = new();
+    private int _matches = 0;
+
+    public bool CheckInclusion(string s1, string s2)
+    {
+        if (s1.Length > s2.Length)
+            return false;
+
+        _dict1 = s1.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count());
+
+        int indexL = 0;
+
+        for (int indexR = 0; indexR < s2.Length; indexR++)
+        {
+            _dict2[s2[indexR]] = _dict2.GetValueOrDefault(s2[indexR], 0) + 1;
+
+            if (indexR - indexL + 1 > s1.Length)
+            {
+                _dict2[s2[indexL]] -= 1;
+                
+                if (_dict2[s2[indexL]] == 0)
+                    _dict2.Remove(s2[indexL]);
+
+                indexL++;
+            }
+
+            if (IsEqual(_dict1, _dict2))
+                return true;
+        }
+
+        return false;
+    }
+
+    private bool IsEqual(Dictionary<char, int> dict1, Dictionary<char, int> dict2)
+    {
+        return dict1.Count == dict2.Count &&
+               dict1.All(item => dict2.TryGetValue(item.Key, out int value) && value == item.Value);
+    }
+}
