5660a19b061edabc87ea581d4d6084d1eb96f320
EnverMT <anvarmtg@gmail.com>
2024-06-05 18:59:41 +0500

    sliding window max

---
+++ LeetCode.Tests/Sliding window/239 Sliding Window Maximum.cs
@@ -0,0 +1,45 @@
+﻿using Leetcode.SlidingWindow._239._Sliding_Window_Maximum;
+
+
+namespace Leetcode.Tests.Sliding_window;
+
+public class _239_Sliding_Window_Maximum
+{
+    Solution _solution = new Solution();
+
+    [Fact]
+    public void _239_Sliding_Window_Maximum_Test_1()
+    {
+        int[] nums = [1, 3, -1, -3, 5, 3, 6, 7];
+        int k = 3;
+        int[] output = [3, 3, 5, 5, 6, 7];
+
+        int[] result = _solution.MaxSlidingWindow(nums, k);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void _239_Sliding_Window_Maximum_Test_2()
+    {
+        int[] nums = [1];
+        int k = 1;
+        int[] output = [1];
+
+        int[] result = _solution.MaxSlidingWindow(nums, k);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void _239_Sliding_Window_Maximum_Test_3()
+    {
+        int[] nums = [1, 2, 1, 0, 4, 2, 6];
+        int k = 3;
+        int[] output = [2, 2, 4, 4, 6];
+
+        int[] result = _solution.MaxSlidingWindow(nums, k);
+
+        Assert.Equal(output, result);
+    }
+}
---
+++ Leetcode/SlidingWindow/239. Sliding Window Maximum/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace Leetcode.SlidingWindow._239._Sliding_Window_Maximum;
+
+public class Solution
+{
+    public int[] MaxSlidingWindow(int[] nums, int k)
+    {
+        int indexL = 0;
+        List<int> result = [];
+        LinkedList<int> q = new();
+
+        for (int indexR = 0; indexR < nums.Length; indexR++)
+        {
+            while (q.Count > 0 && nums[q.Last.Value] < nums[indexR])
+            {
+                q.RemoveLast();
+            }
+
+            q.AddLast(indexR);
+
+            if (indexL > q.First?.Value)
+                q.RemoveFirst();
+
+            if (indexR + 1 >= k)
+            {
+                result.Add(nums[q.First.Value]);
+                indexL++;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
