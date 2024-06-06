50e3b21d44c0133e3d61b99dc13b9296d03faee0
EnverMT <anvarmtg@gmail.com>
2024-06-06 13:59:43 +0500

    add two sum

---
+++ LeetCode.Tests/Linked List/2 Add Two Numbers.cs
@@ -0,0 +1,57 @@
+﻿using Leetcode.Linked_List;
+using Leetcode.Linked_List._2_Add_Two_Numbers;
+
+namespace Leetcode.Tests.Linked_List;
+
+public class _2_Add_Two_Numbers
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void Test_1()
+    {
+        int[] l1 = [2, 4, 3];
+        int[] l2 = [5, 6, 4];
+        int[] output = [7, 0, 8];
+
+        ListNode node1 = ListNode.FromArray(l1);
+        ListNode node2 = ListNode.FromArray(l2);
+
+        ListNode result = _solution.AddTwoNumbers(node1, node2);
+        int[] resultArr = ListNode.ToArray(result);
+
+        Assert.Equal(output, resultArr);
+    }
+
+    [Fact]
+    public void Test_2()
+    {
+        int[] l1 = [0];
+        int[] l2 = [0];
+        int[] output = [0];
+
+        ListNode node1 = ListNode.FromArray(l1);
+        ListNode node2 = ListNode.FromArray(l2);
+
+        ListNode result = _solution.AddTwoNumbers(node1, node2);
+        int[] resultArr = ListNode.ToArray(result);
+
+        Assert.Equal(output, resultArr);
+    }
+
+    [Fact]
+    public void Test_3()
+    {
+        int[] l1 = [9, 9, 9, 9, 9, 9, 9];
+        int[] l2 = [9, 9, 9, 9];
+        int[] output = [8, 9, 9, 9, 0, 0, 0, 1];
+
+        ListNode node1 = ListNode.FromArray(l1);
+        ListNode node2 = ListNode.FromArray(l2);
+
+        ListNode result = _solution.AddTwoNumbers(node1, node2);
+        int[] resultArr = ListNode.ToArray(result);
+
+        Assert.Equal(output, resultArr);
+    }
+}
---
+++ Leetcode/Linked List/2 Add Two Numbers/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode.Linked_List._2_Add_Two_Numbers;
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode node1, ListNode node2)
+    {
+        ListNode head = new(0);
+        ListNode current = head;
+
+        int overflow = 0;
+
+        while (node1 != null || node2 != null || overflow > 0)
+        {
+            int n1 = node1?.val ?? 0;
+            int n2 = node2?.val ?? 0;
+            int n = n1 + n2 + overflow;
+            overflow = n / 10;
+
+            ListNode nextNode = new ListNode(n % 10);
+
+            current.next = nextNode;
+            current = current.next;
+
+            node1 = node1?.next;
+            node2 = node2?.next;
+        }
+
+        return head.next;
+    }
+}
