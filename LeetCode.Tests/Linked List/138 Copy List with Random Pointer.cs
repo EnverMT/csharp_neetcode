fcc919d31165288e72feef88ed93123d3165ef54
EnverMT <anvarmtg@gmail.com>
2024-06-06 10:00:25 +0500

    138 init

---
+++ LeetCode.Tests/Linked List/138 Copy List with Random Pointer.cs
@@ -0,0 +1,48 @@
+﻿using Leetcode.Linked_List;
+using Leetcode.Linked_List._138_Copy_List_with_Random_Pointer;
+
+namespace Leetcode.Tests.Linked_List;
+
+public class _138_Copy_List_with_Random_Pointer
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void Test1()
+    {
+        int?[][] head = [[7, null], [13, 0], [11, 4], [10, 2], [1, 0]];
+        int?[][] output = [[7, null], [13, 0], [11, 4], [10, 2], [1, 0]];
+
+        Node node = Node.FromArray(head);
+        Node resultNode = _solution.CopyRandomList(node);
+        int?[][] result = Node.ToArray(resultNode);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        int?[][] head = [[1, 1], [2, 1]];
+        int?[][] output = [[1, 1], [2, 1]];
+
+        Node node = Node.FromArray(head);
+        Node resultNode = _solution.CopyRandomList(node);
+        int?[][] result = Node.ToArray(resultNode);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        int?[][] head = [[3, null], [3, 0], [3, null]];
+        int?[][] output = [[3, null], [3, 0], [3, null]];
+
+        Node node = Node.FromArray(head);
+        Node resultNode = _solution.CopyRandomList(node);
+        int?[][] result = Node.ToArray(resultNode);
+
+        Assert.Equal(output, result);
+    }
+}
---
+++ Leetcode/Linked List/138 Copy List with Random Pointer/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace Leetcode.Linked_List._138_Copy_List_with_Random_Pointer;
+
+public class Solution
+{
+    public Node CopyRandomList(Node head)
+    {
+        return head;
+    }
+}
