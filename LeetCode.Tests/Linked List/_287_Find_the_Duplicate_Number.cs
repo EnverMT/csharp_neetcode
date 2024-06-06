548f938d8380ccd5e52e176b17938f66e25cca47
EnverMT <anvarmtg@gmail.com>
2024-06-06 14:45:45 +0500

    find duplicate floyd

---
+++ LeetCode.Tests/Linked List/_287_Find_the_Duplicate_Number.cs
@@ -0,0 +1,42 @@
+﻿using Leetcode.Linked_List._287_Find_the_Duplicate_Number;
+
+
+namespace Leetcode.Tests.Linked_List;
+
+public class _287_Find_the_Duplicate_Number
+{
+    private readonly Solution solution = new();
+
+    [Fact]
+    public void Test_1()
+    {
+        int[] nums = [1, 3, 4, 2, 2];
+        int output = 2;
+
+        int result = solution.FindDuplicate(nums);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void Test_2()
+    {
+        int[] nums = [3, 1, 3, 4, 2];
+        int output = 3;
+
+        int result = solution.FindDuplicate(nums);
+
+        Assert.Equal(output, result);
+    }
+
+    [Fact]
+    public void Test_3()
+    {
+        int[] nums = [3, 3, 3, 3, 3];
+        int output = 3;
+
+        int result = solution.FindDuplicate(nums);
+
+        Assert.Equal(output, result);
+    }
+}
---
+++ Leetcode/Linked List/287 Find the Duplicate Number/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode.Linked_List._287_Find_the_Duplicate_Number;
+
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        int slow = 0, fast = 0;
+
+        while (true)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (fast == slow)
+                break;
+        }
+
+        int slow2 = 0;
+        while (true)
+        {
+            slow2 = nums[slow2];
+            slow = nums[slow];
+            if (slow2 == slow)
+                break;
+        }
+
+        return slow2;
+
+    }
+}
