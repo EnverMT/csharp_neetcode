96e7e8ccc2296f86e8f557a90757668451dd7e4c
EnverMT <anvarmtg@gmail.com>
2024-07-15 18:01:30 +0500

    diameter

---
+++ LeetCode.Tests/Tree/543 Diameter of Binary Tree.cs
@@ -0,0 +1,57 @@
+﻿using Leetcode.Tree;
+using Leetcode.Tree._543_Diameter_of_Binary_Tree;
+
+namespace Leetcode.Tests.Tree;
+
+public class _543_Diameter_of_Binary_Tree
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void Test_1()
+    {
+        int?[] root = [1, 2, 3, 4, 5];
+        int result = 3;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        int output = _solution.DiameterOfBinaryTree(rootTreeNode);
+
+        Assert.Equal(result, output);
+    }
+
+    [Fact]
+    public void Test_2()
+    {
+        int?[] root = [1, 2];
+        int result = 1;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        int output = _solution.DiameterOfBinaryTree(rootTreeNode);
+
+        Assert.Equal(result, output);
+    }
+
+    [Fact]
+    public void Test_3()
+    {
+        int?[] root = [1, null, 2, 3, 4, 5];
+        int result = 3;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        int output = _solution.DiameterOfBinaryTree(rootTreeNode);
+
+        Assert.Equal(result, output);
+    }
+
+    [Fact]
+    public void Test_4()
+    {
+        int?[] root = [1, 2, 3];
+        int result = 2;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        int output = _solution.DiameterOfBinaryTree(rootTreeNode);
+
+        Assert.Equal(result, output);
+    }
+}
---
+++ Leetcode/Tree/543 Diameter of Binary Tree/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.Tree._543_Diameter_of_Binary_Tree;
+
+
+public class Solution
+{
+    private int _result = 0;
+
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        Depth(root);
+
+        return _result;
+    }
+
+    private int Depth(TreeNode? node)
+    {
+        if (node == null)
+            return -1;
+
+        int left = Depth(node.left);
+        int right = Depth(node.right);
+
+        _result = Math.Max(_result, 2 + left + right);
+
+        return 1 + Math.Max(left, right);
+    }
+}
+
