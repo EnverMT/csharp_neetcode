4afc801e6f8168c4c0f086ca6e00846a1ab2be97
EnverMT <anvarmtg@gmail.com>
2025-09-15 14:42:01 +0500

    upd

---
+++ LeetCode.Tests/Tree/110 Balanced Binary Tree.cs
@@ -0,0 +1,19 @@
+﻿using Leetcode.Tree;
+using Leetcode.Tree._110_Balanced_Binary_Tree;
+
+namespace Leetcode.Tests.Tree;
+
+public class _110_Balanced_Binary_Tree
+{
+    private readonly Solution _solution = new();
+
+    [Fact]
+    public void _110_Balanced_Binary_Tree_Test_1()
+    {
+        int?[] root = [1, 2, 3, null, null, 4];
+        bool result = true;
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        bool output = _solution.IsBalanced(rootTreeNode!);
+        Assert.Equal(output, result);
+    }
+}
---
+++ Leetcode/Tree/110 Balanced Binary Tree/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace Leetcode.Tree._110_Balanced_Binary_Tree;
+
+public class Solution
+{
+    public bool IsBalanced(TreeNode root)
+    {
+        return false;
+    }
+}
