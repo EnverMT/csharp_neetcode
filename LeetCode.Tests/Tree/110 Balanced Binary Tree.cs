db29baf33b217160d5632f92ad3a1ddfd42b2ecd
EnverMT <anvarmtg@gmail.com>
2025-09-15 15:42:41 +0500

    110 Balanced Binary Tree

--- LeetCode.Tests/Tree/110 Balanced Binary Tree.cs
+++ LeetCode.Tests/Tree/110 Balanced Binary Tree.cs
@@ -16,4 +16,40 @@ public class _110_Balanced_Binary_Tree
         bool output = _solution.IsBalanced(rootTreeNode!);
         Assert.Equal(output, result);
     }
+
+    [Fact]
+    public void _110_Balanced_Binary_Tree_Test_2()
+    {
+        int?[] root = [1, 2, 3, null, null, 4, null, 5];
+        bool result = false;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        bool output = _solution.IsBalanced(rootTreeNode);
+
+        Assert.Equal(result, output);
+    }
+
+    [Fact]
+    public void _110_Balanced_Binary_Tree_Test_3()
+    {
+        int?[] root = [];
+        bool result = true;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        bool output = _solution.IsBalanced(rootTreeNode!);
+
+        Assert.Equal(result, output);
+    }
+
+    [Fact]
+    public void _110_Balanced_Binary_Tree_Test_4()
+    {
+        int?[] root = [1, 2, null, 3];
+        bool result = false;
+
+        TreeNode? rootTreeNode = TreeBase.ConvertArrayToTree(root);
+        bool output = _solution.IsBalanced(rootTreeNode!);
+
+        Assert.Equal(result, output);
+    }
 }
--- Leetcode/Tree/110 Balanced Binary Tree/Solution.cs
+++ Leetcode/Tree/110 Balanced Binary Tree/Solution.cs
@@ -1,9 +1,32 @@
-﻿namespace Leetcode.Tree._110_Balanced_Binary_Tree;
+﻿using Leetcode.Linked_List;
+
+namespace Leetcode.Tree._110_Balanced_Binary_Tree;
 
 public class Solution
 {
-    public bool IsBalanced(TreeNode root)
+    private bool _isBalanced = true;
+
+    public bool IsBalanced(TreeNode? root)
     {
-        return false;
+        if (root == null) return _isBalanced;
+
+        GetHeight(root);
+
+        return _isBalanced;
+    }
+
+    private int GetHeight(TreeNode? node)
+    {
+        if (node?.val == null) return 0;
+
+        int leftHeight = GetHeight(node.left);
+        int rightHeight = GetHeight(node.right);
+
+        if (Math.Abs(leftHeight - rightHeight) > 1)
+        {
+            _isBalanced = false;
+        }
+
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
